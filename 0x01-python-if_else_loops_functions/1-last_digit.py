d96a0f6919f0d78c53d80cd7f965a753affa86cd
Codammy <damisco005@gmail.com>
2023-03-07 09:41:32 -0800

    python control structures

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+t_num = number
+
+if number < 0:
+    t_num *= -1
+ldigit = t_num % 10
+
+print(f"Last digit of {number} is {ldigit}", end=" ")
+
+if ldigit > 5:
+    print("and is greater than 5")
+elif ldigit < 6 and ldigit != 0:
+    print("and is less than 6 and not 0")
+else:
+    print("and is 0")
