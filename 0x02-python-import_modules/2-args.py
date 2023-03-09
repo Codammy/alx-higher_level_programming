044731db2fe044465f51c8406df1c88bf1de9090
Codammy <damisco005@gmail.com>
2023-03-09 13:52:01 -0800

    python modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/bin/usr/python3
+
+from sys import argv
+
+
+def cmdarg(argv):
+    n = len(argv) - 1
+    if n == 1:
+        print("{} argument".format(n), end="")
+    else:
+        print("{} arguments".format(n), end="")
+    if n > 0:
+        print(":")
+    else:
+        print(".")
+
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    cmdarg(argv)
