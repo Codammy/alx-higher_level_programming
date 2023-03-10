16bc95f0eb208a2e3d4d7140f382c82e844da733
Codammy <damisco005@gmail.com>
2023-03-10 03:25:00 -0800

    python modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,22 +6,23 @@ import calculator_1
 
 def runcal(argv):
     ln = len(argv) - 1
+    print(ln, argv[2])
     if ln != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
-    operator = argv[2]
+    operator = str(argv[2])
 
     if operator == '+':
         print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     elif operator == '-':
-        print("{} + {} = {}".format(a, b, calculator_1.sub(a, b)))
+        print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     elif operator == '*':
-        print("{} + {} = {}".format(a, b, calculator.mul(a, b)))
+        print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
     elif operator == '/':
-        print("{} + {} = {}".format(a, b, calculator.div(a, b)))
+        print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
