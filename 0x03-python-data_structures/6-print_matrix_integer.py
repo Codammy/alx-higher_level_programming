43d3852c14cba6a8f7be1c9dc896d5e6d4c47031
Codammy <damisco005@gmail.com>
2023-03-11 04:32:25 -0800

    python data structures,list and tuples

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        sp = 0
+        for j in i:
+            sp += 1
+            print("{}".format(j), end="")
+            if sp != len(i):
+                print(" ", end="")
+        print()
