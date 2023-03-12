f32d3aeccc38f9dbaabeb350b5209a3c23e56140
Codammy <damisco005@gmail.com>
2023-03-12 03:13:40 -0700

    python data structures,list and tuples

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+# /usr/bin/python3
+
+def max_integer(my_list=[]):
+    _cmp = 0
+    for i in my_list:
+        if i > _cmp:
+            _cmp = i
+    if _cmp == 0:
+        return None
+    return _cmp
