e8c5c6886045ac70a3b52fee106cc75112b267b5
Codam <damisco005@gmail.com>
2023-10-04 09:54:54 +0100

    prints abnormal alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for i in range(122, 97):
-    print("{}".format(i))
+i = 122
+while i >= 97:
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i - 32)), end="")
+    i -= 1
