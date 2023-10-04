8e0018d74a320862b7495ee7b2114c2f4fd408c9
Codam <damisco005@gmail.com>
2023-10-04 08:06:39 +0100

    Inventing is a combination of brains and materials. The more brains you use, the less material you need - collaboration

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(99):
-    print()
+for i in range(48, 58):
+    for j in range(i + 1, 58):
+        print("{}{}".format(chr(i), chr(j)), end="")
+        if (i < 56):
+            print(end=", ")
+print()
