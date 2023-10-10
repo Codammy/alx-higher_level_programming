0d7eff59a55fca96f340b7ecc64846bdb64bf585
Codam <damisco005@gmail.com>
2023-10-10 05:58:52 +0100

    not completed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-add_tuple = __import__('7-add_tuple').add_tuple
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_t1 = len(tuple_a)
+    len_t2 = len(tuple_b)
+    if (len_t1 < 2 or len_t2 < 2):
+        if len_t1== 1:
+            b1 = 0
+        if len(tuple_a) == 0:
+            a1 == 0
+        if len(tuple_b) == 1:
+            b2 = 0
+        if len(tuple_b) == 0:
+            a2 == 0
+    else:
+        a
+    return (a1 + a2, b1 + b2)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-multiple_returns = __import__('8-multiple_returns').multiple_returns
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+def multiple_returns(sentence):
+    a = None
+    ls = len(sentence)
+    if ls >= 1:
+        a = sentence[0]
+    return ls, a
