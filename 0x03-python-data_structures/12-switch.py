a4c20031b8e15655779b3c9c1a72e42be2dda37f
Codam <damisco005@gmail.com>
2023-10-10 21:44:49 +0100

    Cc in string

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-a, b = b, a # A simple example of tuple unpacking
+a, b = b, a  # A simple example of tuple unpacking
 print("a={:d} - b={:d}".format(a, b))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,15 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     len_t1 = len(tuple_a)
     len_t2 = len(tuple_b)
-    if (len_t1 < 2 or len_t2 < 2):
-        if len_t1== 1:
-            b1 = 0
-        if len(tuple_a) == 0:
-            a1 == 0
-        if len(tuple_b) == 1:
-            b2 = 0
-        if len(tuple_b) == 0:
-            a2 == 0
-    else:
-        a
-    return (a1 + a2, b1 + b2)
+    if len_t1 == 0:
+        tuple_a = 0, 0
+    elif len_t1 == 1:
+        tuple_a = tuple_a[0], 0
+    if len_t2 == 0:
+        tuple_b = 0, 0
+    elif len_t2 == 1:
+        tuple_b = tuple_b[0], 0
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
