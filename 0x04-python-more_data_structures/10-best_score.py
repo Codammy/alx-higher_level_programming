df8e11b4c8298ccedf9dbc811bca2b96ee75555d
Codam <damisco005@gmail.com>
2023-10-15 18:23:08 +0100

    10-b*

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary is {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     bestScoreIndex = 0
     bestScore = 0
