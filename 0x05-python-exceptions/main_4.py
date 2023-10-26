0df1a19f302d8536ee096bc4b136dbfdbfa1e4f0
Codam <damisco005@gmail.com>
2023-10-26 01:49:42 +0100

    4-list_division.py

---
+++ 0x05-python-exceptions/main_4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+list_division = __import__('4-list_division').list_division
+
+my_list_1 = [10, 0, 4]
+my_list_2 = [2, 4, 0] 
+list_length = 3
+result = list_division(my_list_1, my_list_2, list_length)
+print(result)
