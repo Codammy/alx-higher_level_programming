80ad1812fb7b305a84652e0923eaf995ce903337
Codam <damisco005@gmail.com>
2023-11-21 01:31:47 +0100

    new test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,9 +5,10 @@
 def matrix_divided(matrix, div):
     """divides all elements of a matrix"""
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
     if type(div) is not int:
-       raise TypeError("div must be a number")
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     ln = len(matrix[0])
@@ -15,12 +16,14 @@ def matrix_divided(matrix, div):
     for row in matrix:
         child = []
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            " of integers/floats")
         if ln != len(row):
             raise TypeError("Each row of the matrix must have the same size")
         for r in row:
             if type(r) is not int and type(r) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats")
             child.append(float(f"{r / div:.2f}"))
         nwlist.append(child)
         ln = len(row)
