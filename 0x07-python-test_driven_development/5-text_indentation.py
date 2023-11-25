a38f65d72afe3f2a795a8747a62af58587da1153
Codam <damisco005@gmail.com>
2023-11-25 12:32:15 +0100

    tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,13 @@ def text_indentation(text):
     """prints a text with 2 new lines after . ? and : """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    for i in text:
-        print(i, end="")
-        if i == '.' or i == '?' or i == ':':
+    ln = len(text)
+    i = 0
+    while i < ln:
+        c = text[i]
+        print(c, end="")
+        i += 1
+        if c == '.' or c == '?' or c == ':':
             print('\n')
+            if text[i] == ' ':
+                i += 1
