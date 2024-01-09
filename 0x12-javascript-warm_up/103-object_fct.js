7b9727c85fc7d5dd984c0ed259cb8fb5590ac576
Codam <damisco005@gmail.com>
2024-01-09 15:54:13 +0100

    101-call_me_moby.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
+myObject.incr = function () { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
