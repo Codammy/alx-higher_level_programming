393e23d3fb937daa048a93e38b270286ebe55bd9
Codam <damisco005@gmail.com>
2024-01-09 21:23:08 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,7 +7,7 @@ if (len < 4) { console.log(0); } else {
   let n = 0;
   while (n < len) {
     for (let i = len - 1; i > 2; i--) {
-      if (argv[i] > argv[i - 1]) {
+      if (Number(argv[i]) > Number(argv[i - 1])) {
         big = argv[i];
         argv[i] = argv[i - 1];
         argv[i - 1] = big;
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const proc = require('process');
 const argLen = proc.argv.length;
-if (argLen === 3) { console.log('Argument found'); } else if (argLen > 3) { console.log('Arguments found'); } else { console.log('No Argument'); }
+if (argLen === 3) { console.log('Argument found'); } else if (argLen > 3) { console.log('Arguments found'); } else { console.log('No argument'); }
