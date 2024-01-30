832ea6a3ff2ae6e0ba9da1d34c3c62928e0a045e
Codam <damisco005@gmail.com>
2024-01-30 16:16:05 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
+# takes in a URL, sends a request to that URL.
 curl -sIX ALLOW $1 | grep Allow | cut -c -8-
