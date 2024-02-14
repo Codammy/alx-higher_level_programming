3defa8a8846ab0b49b657e7fcce2f3db792cfe99
Dami <damisco005@gmail.com>
2024-02-14 06:00:05 +0100

    4-rectangle.js 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const stdout = require("process").stdout;
+class Rectangle{
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+		this.width = w;
+		this.height = h;
+		}
+	}
+	print() {
+		for (let i = 0; i < this.height; i++) {
+			for (let j = 0; j < this.width; j++) {
+				stdout.write("X");
+			}
+			console.log("");
+		}
+	}
+	rotate() {
+		let temp = this.width;
+		this.width = this.height
+		this.height = temp;
+	}
+}
+module.exports = Rectangle
