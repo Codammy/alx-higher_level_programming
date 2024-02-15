d300b0259fc50aa844e215034804245bced58a48
Codam <damisco005@gmail.com>
2024-02-15 14:00:29 +0100

    new commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,11 @@ const { argv } = require('process');
 const arr = argv.slice(2);
 for (let i = 0; i < arr.length; i++) {
   for (let j = arr.length - 1; j > 0; j--) {
-    if (arr[j] > arr[j - 1]) {
+    if (Number(arr[j]) > Number(arr[j - 1])) {
       const tmp = arr[j];
       arr[j] = arr[j - 1];
       arr[j - 1] = tmp;
     }
   }
 }
-console.log(arr[1] ? arr[1] : arr[0]);
+console.log(arr[1] ? arr[1] : 0);
