b5f1835919a54fcb85a81a000131e343e00d38a1
Dami <damisco005@gmail.com>
2024-02-19 17:01:00 +0100

    if __name__ == __main__

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,9 @@
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    # user, passwd, db = argv[1:]
-    Engine = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    user, passwd, db = argv[1:]
+    Engine = MySQLdb.connect(host="localhost", port=3306, user=user,
+                             passwd=passwd, db=db)
     cursor = Engine.cursor()
     cursor.execute('select * from states order by id asc')
     for row in cursor.fetchall():
