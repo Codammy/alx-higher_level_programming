0b4cf6ed7ae2734bf221b6285e5b87067c3b0a30
Dami <damisco005@gmail.com>
2024-02-19 16:09:38 +0100

    0-select_states.py 1-filter_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
+
+
 import MySQLdb
 import sys
-
-
 user, passwd, db = sys.argv[1:]
 Engine = MySQLdb.connect(host="localhost", port=3306, user=user,
                          passwd=passwd, db=db)
