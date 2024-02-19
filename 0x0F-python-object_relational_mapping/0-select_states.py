2feaad290e13ede60c1ce5f8fd858c236be0a591
Dami <damisco005@gmail.com>
2024-02-19 16:59:28 +0100

    __pycache__ 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,15 @@
 """ lists all states from the database hbtn_0e_0_usa """
 
 
-import MySQLdb
-import sys
-user, passwd, db = sys.argv[1:]
-Engine = MySQLdb.connect(host="localhost", port=3306, user=user,
-                         passwd=passwd, db=db)
-cursor = Engine.cursor()
-cursor.execute('select * from states order by id asc')
-for row in cursor.fetchall():
-    print(row)
-cursor.close()
-Engine.close()
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    # user, passwd, db = argv[1:]
+    Engine = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = Engine.cursor()
+    cursor.execute('select * from states order by id asc')
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    Engine.close()
