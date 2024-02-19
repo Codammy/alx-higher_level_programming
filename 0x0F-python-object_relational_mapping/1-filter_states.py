ab2dd9fe66c840d3c00e9e81f0f1b7cf1624ac32
Dami <damisco005@gmail.com>
2024-02-19 16:05:44 +0100

    0-select_states.py 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""
+ lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
-
 user, passwd, db = sys.argv[1:]
-Engine = MySQLdb.connect(host="localhost", port=3306, user=user,
+conn = MySQLdb.connect(host="localhost", port=3306, user=user,
                          passwd=passwd, db=db)
-cursor = Engine.cursor()
+cursor = conn.cursor()
 cursor.execute('select * from states order by id asc')
 for row in cursor.fetchall():
     print(row)
 cursor.close()
-Engine.close()
+conn.close()
