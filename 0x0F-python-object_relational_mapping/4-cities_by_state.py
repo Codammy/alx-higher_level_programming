9a0a3764be5432c57b9de970cdaa39d613c27b46
Dami <damisco005@gmail.com>
2024-02-24 08:07:45 +0100

    Dallas, Houston, Austin

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
     user, passwd, db = argv[1:]
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user, db=db, passwd=passwd)
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=user, db=db, passwd=passwd)
     cur = conn.cursor()
-    cur.execute("select cities.id, cities.name ,states.name from cities, states where cities.state_id=states.id order by cities.id asc")
+    cur.execute("select cities.id, cities.name, states.name from cities,\
+                states where cities.state_id=states.id order by cities.id asc")
     for data in cur.fetchall():
         print(data)
-
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
+takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
 """
 
 
@@ -11,14 +12,16 @@ if __name__ == "__main__":
     usr, passwd, db, state = argv[1:]
     conn = MySQLdb.connect(user=usr, passwd=passwd, db=db)
     cur = conn.cursor()
-    # cur.execute('select cities.name from cities, states where states.name=%s and states.id=cities.state_id', (state, ))
     """ or """
     try:
-        cur.execute('select cities.name from cities join states on states.id=cities.state_id and states.name=%s', (state, ))                                      
+        cur.execute('select cities.name from cities join states on \
+                    states.id=cities.state_id and states.name=%s order\
+                    by cities.id asc', (state, ))
         cities_in_state = cur.fetchall()
         len = len(cities_in_state) - 1
         for city in cities_in_state:
-            print(city[0], end=('' if city[0] == cities_in_state[len][0] else ', '))
+            print(city[0], end=('' if city[0] ==
+                  cities_in_state[len][0] else ', '))
     except Exception:
         pass
     print()
