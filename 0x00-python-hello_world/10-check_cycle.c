58c122e6b74fa24662b8fd1159209a5365607108
Dami <damisco005@gmail.com>
2024-03-04 10:05:25 +0100

    10-check_cycle.c cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,23 +2,26 @@
 #include <stdio.h>
 
 /**
- * check_cycle - checks for a cycle in a linked list
+ * check_cycle - checks for a palindrome in a linked list
  * @list: linked list
  *
- * Return: 1 if there is a cycle, 0 otherwise
+ * Return: 1 on Success
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *tortoise = list, *hare = list;
+	listint_t *curr = list, *head = list;
 
-    while (hare && hare->next)
-    {
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-
-        if (tortoise == hare)
-            return (1);
-    }
-
-    return (0);
+	while (curr)
+	{
+		if (curr->next == list)
+			return (1);
+		if (curr == list)
+		{
+			list = head;
+			curr = curr->next;
+			continue;
+		}
+		list = list->next;
+	}
+	return (0);
 }
