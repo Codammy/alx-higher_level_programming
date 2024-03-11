8b06a1d7a0864e3dde9a47b443e0b9a660a7e845
Dami <damisco005@gmail.com>
2024-03-11 23:08:58 +0100

    6-multi_languages_loop.js 5-to_integer.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+const argv = require('process').argv
+argv[2] ? console.log(argv[2]) : console.log('No argument')
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const argv = require('process').argv
+let number = parseInt(argv[2])
+console.log(`${ number  ? `My number: ${number}` : 'Not a number'}`)
