becd88548ceed9b8391b9aef9121f15d2c763c62
Codam <damisco005@gmail.com>
2024-03-12 23:55:49 +0100

    advanced first

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,14 +2,12 @@
 
 const dict = require('./101-data').dict;
 const newObj = {};
-for (let k in dict) {
-	let v = dict[k];
-	if (newObj.hasOwnProperty(v)) {
-		newObj[v].push(k);
-	}
-	else {
-		newObj[v] = [ k ];
-	}
+for (const k in dict) {
+  const v = dict[k];
+  if (newObj[v]) {
+    newObj[v].push(k);
+  } else {
+    newObj[v] = [k];
+  }
 }
 console.log(newObj);
-
