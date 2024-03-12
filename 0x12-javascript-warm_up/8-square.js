7f9ccf1fff624c2c0da431e2a225162cee6992f3
Codam <damisco005@gmail.com>
2024-03-12 21:05:26 +0100

    semistandard --fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,6 @@ const number = parseInt(argv[2]);
 let c = number * number;
 let str = '';
 if (!number) { console.log('Missing size'); } else {
-  r = 0;
   while (c > 0) {
     str += 'X';
     c--;
