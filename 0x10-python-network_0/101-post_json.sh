00b65d4bb904a385c49b9ef4640f3b61ea223a6f
Dami <damisco005@gmail.com>
2024-04-09 06:47:03 +0100

    101-post_json.sh 100_status_code.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends json post request from a file to a passed url(argument).
-curl -s-H "Content-Type: application/json" -X POST -d @$1 $2
+curl -sH "Content-Type: application/json" -X POST -d @$1 $2
