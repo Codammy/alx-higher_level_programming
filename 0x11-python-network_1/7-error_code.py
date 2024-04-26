11afb2ab92de3e6b1be85c075cda2ca62443350d
Codam <damisco005@gmail.com>
2024-04-26 20:17:52 +0100

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the
+URL and displays the body of the response.
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    res = requests.get(sys.argv[1])
+    if not res.ok:
+        print(f'Error code: {res.status}')
+    else:
+        print(res.text)
