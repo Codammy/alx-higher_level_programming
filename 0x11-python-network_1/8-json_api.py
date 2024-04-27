3a918fe7496456887571274ea43794897e8fbc66
Dami <damisco005@gmail.com>
2024-04-27 05:19:13 +0100

    8-json_api.py 4-hbtn_status.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    h = {'q': '' if len(sys.argv) < 2 else sys.argv[1]}
+    res = requests.post('http://0.0.0.0:5000/search_user', headers=h)
+    if res.headers['Content-Type'] == 'application/json':
+        js = res.json()
+        if len(js):
+            print(f'[js["id"]] js["name"]')
+        else:
+            print('No result')
+    else:
+        print('Not a valid JSON')
