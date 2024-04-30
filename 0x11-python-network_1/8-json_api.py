ed550d8a37fe8f6904e0ea7a4dbc8784326a9bca
Dami <damisco005@gmail.com>
2024-04-30 17:51:20 +0100

    8-json_api.py 5-hbtn_header.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,12 +10,12 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    p = {'q': '' if len(sys.argv) < 2 else sys.argv[1]}
-    res = requests.post('http://0.0.0.0:5000/search_user', params=p)
+    data = {'q': '' if len(sys.argv) < 2 else sys.argv[1]}
+    res = requests.post('0.0.0.0:5000/search_user', data=data)
     if res.headers['Content-Type'] == 'application/json':
         js = res.json()
         if len(js):
-            print(f'[js["id"]] js["name"]')
+            print(f'{[js["id"]]} {js["name"]}')
         else:
             print('No result')
     else:
