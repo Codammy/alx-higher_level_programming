be36863bf010c048b3b5eac3e8c8c22f533716bb
Dami <damisco005@gmail.com>
2024-05-03 10:45:02 +0100

    100-github_commits.py 10-my_github.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ Python script that takes 2 arguments in order to solve this challenge.
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    owner, repo = sys.argv[1:]
+    r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits',
+                     params={"per_page": 10}
+                     )
+    if r.ok:
+        for data in r.json():
+            sha = data['sha']
+            commit = data['commit']
+            print('{}: {}'.format(sha, commit['author']['name']))
