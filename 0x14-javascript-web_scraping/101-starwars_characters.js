759032c43d358b18f4106ab1b48a24eb1b77d2ff
Dami <damisco005@gmail.com>
2024-05-23 05:48:07 +0100

    100-starwars_characters.js 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,17 +5,17 @@ const fs = require('fs');
 const movie_id = require('process').argv[2];
 
 request('https://swapi-api.alx-tools.com/api/films/' + movie_id, (err, res, dt) => {
-	if (err) {
-	console.log(err);
-	} else {
-	const films = JSON.parse(dt);
-	for (const c_url of films.characters) {
-		request(c_url, (err, res, data) => {
-			if (!err) {
-				const c = JSON.parse(data);
-				console.log(c.name);
-			}
-		});
-	}
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    const films = JSON.parse(dt);
+    for (const c_url of films.characters) {
+      request(c_url, (err, res, data) => {
+        if (!err) {
+          const c = JSON.parse(data);
+          console.log(c.name);
+        }
+      });
+    }
+  }
 });
