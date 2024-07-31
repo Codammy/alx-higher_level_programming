478750c6d4ddb45a1c431545f50e7eaf95e98fed
Dami <damisco005@gmail.com>
2024-07-31 05:43:19 +0100

    0x00-python-hello_world 0x0E-SQL_more_queries

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const fs = require('fs');
-const [filepath, text] = [...require('process').argv.slice(2, 4)];
-
-fs.writeFile(filepath, text, 'utf-8', (err) => {
-  if (err) { console.log(err); }
-});
--- 0x14-javascript-web_scraping/5-request_store.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const request = require('request');
-const fs = require('fs');
-const [URL, path] = [...require('process').argv.slice(2, 4)];
-
-request(URL).pipe(fs.createWriteStream(path));
